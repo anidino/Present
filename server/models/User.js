106a2c7763f1419581d6c3dd46a4e42b78ecc88c
ryanjolley12 <ryanswims@gmail.com>
2021-10-19 22:10:25 -0400

    user.js fixed

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 const Photo = require("./Photo");
+​
 const userSchema = new Schema(
   {
     username: {
@@ -61,20 +62,26 @@ const userSchema = new Schema(
     },
   }
 );
+​
 // set up pre-save middleware to create and hash password
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
+​
   next();
 });
+​
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
+​
 userSchema.virtual("songCount").get(function () {
   return this.songs.length;
 });
+​
 const User = model("User", userSchema);
+​
 module.exports = User;
