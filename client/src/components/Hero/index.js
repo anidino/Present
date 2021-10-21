54461b842610fc134c6b9edff98b29fdc4302451
ryanjolley12 <ryanswims@gmail.com>
2021-10-21 11:55:55 -0400

    added hero

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <section>
+            <h1 id="hero"> Hero section here</h1>
+        </section>
+    );
+
+}
+
+export default Hero;
