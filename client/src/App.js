eede1621e0d626b0553db7fdc6d76541099d7fc2
ryanjolley12 <ryanswims@gmail.com>
2021-10-21 12:13:54 -0400

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Login from './components/Login';
+import Nav from './components/Nav';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +10,8 @@ function App() {
     <div>
       <main>
         <Hero></Hero>
+        <Login></Login>
+        <Nav></Nav>
       </main>
     </div>
   );
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,57 +1,80 @@
 import React from "react";
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
 
 function Nav() {
-
-  function showNavigation() {
-    if (Auth.loggedIn()) {
-      return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/penpals">
-              Penpals
-            </Link>
-          </li>
-          <li className="mx-1">
-            {}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
-      );
-    } else {
-      return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
-      );
-    }
-  }
-
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/playlists">
-         Playlists
-        </Link>
-      </h1>
+    <section>
+      <h1 id="Nav"> Nav bar here</h1>
+      <ul className="flex-row">
 
-      <nav>
-        {showNavigation()}
-      </nav>
-    </header>
+        <li className="mx-1">
+          Profile</li>
+        <li>Donate</li>
+        <li>My memories</li>
+        <li className="mx-1">Penpals
+          </li>
+      </ul>
+    </section>
   );
+
 }
 
 export default Nav;
+
+
+
+// import Auth from "../../utils/auth";
+// import { Link } from "react-router-dom";
+
+// function Nav() {
+
+//   function showNavigation() {
+//     if (Auth.loggedIn()) {
+//       return (
+//         <ul className="flex-row">
+//           <li className="mx-1">
+//             <Link to="/penpals">
+//               Penpals
+//             </Link>
+//           </li>
+//           <li className="mx-1">
+//             {}
+//             <a href="/" onClick={() => Auth.logout()}>
+//               Logout
+//             </a>
+//           </li>
+//         </ul>
+//       );
+//     } else {
+//       return (
+//         <ul className="flex-row">
+//           <li className="mx-1">
+//             <Link to="/signup">
+//               Signup
+//             </Link>
+//           </li>
+//           <li className="mx-1">
+//             <Link to="/login">
+//               Login
+//             </Link>
+//           </li>
+//         </ul>
+//       );
+//     }
+//   }
+
+//   return (
+//     <header className="flex-row px-1">
+//       <h1>
+//         <Link to="/playlists">
+//          Playlists
+//         </Link>
+//       </h1>
+
+//       <nav>
+//         {showNavigation()}
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Nav;
