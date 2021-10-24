f7024d84061422953606da64a47e9130a928ea81
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2021-10-24 17:53:49 -0400

    installed dayjs and jquery via node, refactored

--- client/src/components/Time/index.js
+++ client/src/components/Time/index.js
@@ -1,28 +1,25 @@
 import React from 'react';
-//import btn from "react-bootstrap/btn";
+import $ from 'jquery';
 
 var dayjs = require('dayjs');
 var calendar = require('dayjs/plugin/calendar');
 dayjs.extend(calendar);
 
-//dayjs().format();
 
-
-var now = dayjs()
-var currentDate = dayjs().format('dddd, DD MMMM YYYY');
-var currentHour = dayjs().format('h');
-var testHour = dayjs().hour();
-var currentMinute = dayjs().format('mm');
-var current12 = dayjs().format('A');
 
 
 function Time() {
-    $('#time').text(currentHour + ":" + currentMinute + current12 + ' ' + currentDate)
-        .fadeIn(5000); 
-    return (
-        <section>
-            <btn id="time"> Time and Date Here</btn>
-        </section>
+    //var now = dayjs()
+    var currentDate = dayjs().format('dddd, DD MMMM YYYY');
+    var currentHour = dayjs().format('h');
+    var currentMinute = dayjs().format('mm');
+    var current12 = dayjs().format('A');
+        $('#time').text(currentHour + ":" + currentMinute + current12 + ' ' + currentDate)
+            .fadeIn(5000); 
+        return (
+            <section>
+                 <btn id="time"> Time and Date Here</btn>
+            </section>
     );
 
 }
