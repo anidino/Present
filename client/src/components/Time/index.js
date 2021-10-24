c33e9419e7e359d4ce6e495bb2e0a73b252098cd
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2021-10-24 13:12:54 -0400

    figuring out time/date placement

--- client/src/components/Time/index.js
+++ client/src/components/Time/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
+//import btn from "react-bootstrap/btn";
+
+var dayjs = require('dayjs');
+var calendar = require('dayjs/plugin/calendar');
+dayjs.extend(calendar);
+
+//dayjs().format();
+
+
+var now = dayjs()
+var currentDate = dayjs().format('dddd, DD MMMM YYYY');
+var currentHour = dayjs().format('h');
+var testHour = dayjs().hour();
+var currentMinute = dayjs().format('mm');
+var current12 = dayjs().format('A');
+
 
 function Time() {
+    $('#time').text(currentHour + ":" + currentMinute + current12 + ' ' + currentDate)
+        .fadeIn(5000); 
     return (
         <section>
             <btn id="time"> Time and Date Here</btn>
