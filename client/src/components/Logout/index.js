cb07a9f76f6aa316b6d9cf82446e79fe92b292f2
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2021-10-24 18:51:29 -0400

    trying to remove JSON web tokens

--- client/src/components/Logout/index.js
+++ client/src/components/Logout/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 
+function removeToken() {
+  document.cookie='toekn=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  localStorage.removeItem('token');
+  sessionStorage.removeItem('token');
+}
+
 function Logout() {
+  removeToken();
     return (
 <section className="my-5">
   <h1 id="logout">Are you sure you want to logout?</h1>
