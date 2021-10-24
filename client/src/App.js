33a5477b955a43f2faec5f23cb0f23b4b19705c8
AniElaina <ani.elaina.carr@gmail.com>
2021-10-24 13:23:37 -0400

    installed @apollo/client graphql at root of client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import Profile from './components/Profile';
-import Time from './components/Time';
-import Quote from './components/Quote';
-import Nav from './components/Nav';
-import Search from './components/Search';
-import Gallery from './components/Gallery';
-import Donate from './components/Donate';
-import Logout from './components/Logout';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from "react";
+import Profile from "./components/Profile";
+import Time from "./components/Time";
+import Quote from "./components/Quote";
+import Nav from "./components/Nav";
+import Search from "./components/Search";
+import Gallery from "./components/Gallery";
+import Donate from "./components/Donate";
+import Logout from "./components/Logout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
@@ -17,13 +31,12 @@ function App() {
       <Nav></Nav>
       <Time></Time>
       <Quote></Quote>
-    
+
       <main>
-      <Search></Search>
-      <Gallery></Gallery>
-      <Donate></Donate>
-      <Profile></Profile>
-    
+        <Search></Search>
+        <Gallery></Gallery>
+        <Donate></Donate>
+        <Profile></Profile>
       </main>
       <div>
         <Logout></Logout>
--- server/models/Playlist.js
+++ server/models/Playlist.js
@@ -1,39 +1,39 @@
-const { Schema, model } = require("mongoose");
-const songReactionSchema = require("./SongReaction");
-const dateFormat = require("../utils/dateFormat");
+// const { Schema, model } = require("mongoose");
+// const songReactionSchema = require("./SongReaction");
+// const dateFormat = require("../utils/dateFormat");
 
-const songSchema = new Schema(
-  {
-    songUrl: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (timestamp) => dateFormat(timestamp),
-    },
-    username: {
-      type: String,
-      ref: "User",
-    },
-    user_id: {
-      type: String,
-      ref: "User",
-    },
-    reactions: [songReactionSchema],
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-  }
-);
+// const songSchema = new Schema(
+//   {
+//     songUrl: {
+//       type: String,
+//       required: true,
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//       get: (timestamp) => dateFormat(timestamp),
+//     },
+//     username: {
+//       type: String,
+//       ref: "User",
+//     },
+//     user_id: {
+//       type: String,
+//       ref: "User",
+//     },
+//     reactions: [songReactionSchema],
+//   },
+//   {
+//     toJSON: {
+//       getters: true,
+//     },
+//   }
+// );
 
-songSchema.virtual("songReaction").get(function () {
-  return this.reactions.length;
-});
+// songSchema.virtual("songReaction").get(function () {
+//   return this.reactions.length;
+// });
 
-const Song = model("Song", songSchema);
+// const Song = model("Song", songSchema);
 
-module.exports = Song;
+// module.exports = Song;
