ee6e48b9fee6d74c8174e20c70290616e8686339
ryanjolley12 <ryanswims@gmail.com>
2021-10-24 12:57:59 -0400

    cleaned up

--- client/src/components/Logout/index.js
+++ client/src/components/Logout/index.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 function Logout() {
     return (
 <section className="my-5">
-  <h4 class="subhead">Are you sure you want to log out?</h4>
+  <h4 class="subhead" id="logout">Are you sure you want to log out?</h4>
   <button type="button" class="btn btn-secondary btn-lg btn-block">Log Out</button>
 
 </section>
--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Search = () => {
   return (
-    <section className="my-5">
-  <h4 class="subhead">My Profile</h4>
-    <Container>
-    <button type="button" class="btn btn-secondary btn-lg btn-block">Explore Music</button>
-    </Container>
-    </section>
-  );
-}
+<section className="my-5">
+<h4 class="subhead" id="playlists">My Playlists</h4>
+  <Container>
+  <button type="button"  class="btn btn-secondary btn-lg btn-block">Explore Music</button>
+  </Container>
+  </section>
+);
+};
+
+
 
 export default Search;
