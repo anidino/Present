da3ca03cad5e3b403f6e047bae037034ecff47e9
AniElaina <ani.elaina.carr@gmail.com>
2021-10-25 14:02:34 -0400

    reactRouter implemented. npm i react-router-dom and jwt-decode

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,10 @@
-
-import React from 'react';
-import Profile from './components/Profile';
-import Time from './components/Time';
-import Quote from './components/Quote';
-import Nav from './components/Nav';
-import Gallery from './components/Gallery';
-import Donate from './components/Donate';
-import Logout from './components/Logout';
-import Logo from './components/Logo';
-import Playlists from './components/Playlists';
-import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
 
 // const httpLink = createHttpLink({
 //   uri: "http://localhost:3001/graphql",
@@ -26,33 +18,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div>
-      <Nav></Nav>
-
-      </div>
-
-
-      <div>
-      <Logo></Logo>
-     
-      <Time></Time>
-      </div>
-    
-      <main>
-      <Playlists></Playlists>
-      <Gallery></Gallery>
-      <Quote></Quote>
-      <Donate></Donate>
-      <Profile></Profile>
-    
-
-      </main>
-      <div>
-        <Logout></Logout>
-      </div>
-      <footer><Footer></Footer></footer>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        {/* <Route exact path="/signup" component={Signup} /> */}
+      </Switch>
     </div>
- 
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+
+//set up router to access browser router in app
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
