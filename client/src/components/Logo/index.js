e444a420a2e849829fe48ed1d23f814abf73d10e
ryanjolley12 <ryanswims@gmail.com>
2021-10-24 22:00:29 -0400

    jumbotron

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -6,9 +6,9 @@ const Logo = (props) => {
               <div id="logo-box">
                 <Jumbotron>
                   <h1 className="display-3" class="present" id="present"><span role="img" aria-label="music"> ♫</span>Present</h1>
-                  <p className="lead">Interaction made simple. See inspirational quotes, listen to your favorite music, and donate to a worthy cause.</p>
+                  <p className="lead" class="jumbotext">Interaction made simple. See inspirational quotes, listen to your favorite music, and donate to a worthy cause.</p>
                   <hr className="my-2" />
-                  <p>A simple web application that helps users struggling with memory loss and mental illness through the healing power of music.</p>
+                  <p class="jumbotext">A simple web application that helps users struggling with memory loss and mental illness through the healing power of music.</p>
                 </Jumbotron>
               </div>
             );
