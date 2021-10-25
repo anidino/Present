d90ba8cadd4f12d1e7fd5c68437b417fb6029335
ryanjolley12 <ryanswims@gmail.com>
2021-10-25 15:16:47 -0400

    added time/date; formatted modal

--- client/src/components/Donate/index.js
+++ client/src/components/Donate/index.js
@@ -5,6 +5,7 @@ function Donate() {
 <section className="my-5">
   <h4 class="subhead" id="donate">Donate</h4>
 
+  <button class="quote-button">Donate to Alzheimer's Association Here</button>
 </section>
     );
 
--- client/src/components/Time/index.js
+++ client/src/components/Time/index.js
@@ -1,11 +1,16 @@
+import moment from 'moment';
 import React from 'react';
 
-function Time() {
+
+const day = moment().format("dddd");
+const date = moment().format("MMMM D, YYYY");
+
+function Time(props) {
+
     return (
         <section>
-           <h2 id="time">Time/Date Here</h2>
+            <h2 id="time">Today is {day} {date}. </h2>
         </section>
-
     );
 
 }
